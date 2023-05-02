ea4c5573b98c247990a9b9d3589b2809035c7e0e
Zachary Meyer <Zachary.Meyer@libertymutual.com>
2023-05-02 10:16:31 -0700

    Refactor key entering logic

--- Wordle/wordle.js
+++ Wordle/wordle.js
@@ -1,15 +1,6 @@
 const testWord = ['W', 'O', 'R', 'D', 'S']
-let currentRow = 1;
-let currentColumn = 0;
+let currentRow = 0;
 currentWord = [];
-
-function addLetterToCurrentWord(value) {
-    currentWord.push(value);
-}
-
-function removeLastLetterFromCurrentWord() {
-    currentWord.length > 0 && currentWord.pop();
-}
 
 function isDeleteKey(value) {
     return (value === 'Delete' || value === 'Backspace')
@@ -19,30 +10,55 @@ function isAlphaKey(value) {
     return value.slice(0, 3) === "Key"
 }
 
-function addLetterFromCurrentRow(value) {
-    if  (currentWord.length < 5) {
-        addLetterToCurrentWord(value)
-    } else {
-        removeLastLetterFromCurrentWord()
-        addLetterToCurrentWord(value)
+function removeLastLetterFromWord(wordArray) {
+    return (wordArray.length >= 0) && wordArray.slice(0, -1)
+}
+
+function addLetterToCurrentWord(value, wordArray) {
+    return ((wordArray.length < 5) ? wordArray : removeLastLetterFromWord(wordArray)).concat([value]) ;
+
+}
+
+function updateCell(row, wordArray) {
+        const $container = document.querySelector("#board-container").children
+        const $row = $container.item(row).children
+
+        Array.from($row)
+            .forEach((child, index) => 
+                {
+                    (index <= wordArray.length) 
+                    ? child.textContent = wordArray[index] 
+                    : child.textContent = '';
+                });
+}
+
+function parseKeyPress(event) {
+    const keycode = event.code;
+    const keyvalue = event.key.toUpperCase();
+
+    return {
+        "keycode" : keycode,
+        "keyvalue" : keyvalue
     }
 }
 
-
-window.addEventListener("keydown", (e) => {
-    enteredKey = e.key.toUpperCase();
-
-    isDeleteKey(e.code) && removeLastLetterFromCurrentWord();
-
-    isAlphaKey(e.code) && addLetterFromCurrentRow(enteredKey);
-    
-    
-    let currentCell = document.querySelector(`.row-1>.column-${currentWord.length}`);
-
-    
-        
-    currentCell.textContent = currentWord.slice(-1);
-    
+function updateWord(keyevent, word) {
+    if (keyevent.keycode && keyevent.keyvalue) {
+        return (isAlphaKey(keyevent.keycode)) 
+            ? addLetterToCurrentWord(keyevent.keyvalue, word) 
+            : (isDeleteKey(keyevent.keycode)) ? removeLastLetterFromWord(word) 
+            : null;
+    } else return;
+}
 
 
-})
+function keyPressHandler(event) {
+   const keyevent = parseKeyPress(event);
+   currentWord = updateWord(keyevent, currentWord);
+   updateCell(currentRow, currentWord)
+
+
+
+   
+}
+window.addEventListener("keydown", (e) => {keyPressHandler(e)})
